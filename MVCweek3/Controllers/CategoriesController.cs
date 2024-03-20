7bf802b7c4e2e9800449517308aee7f17e02b668
deline-jpg <164065072+deline-jpg@users.noreply.github.com>
2024-03-20 12:54:13 +0200

    Add project files.

---
+++ MVCweek3/Controllers/CategoriesController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCweek3.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //Edit
+        public IActionResult Edit(int ? id)
+        {
+            //return view
+            if (id.HasValue)
+                return new ContentResult { Content = id.ToString() };
+            else
+                return new ContentResult { Content = "Not Found Content" };
+        }
+    }
+}
